ebaa8047580f707e966b8299e17bd3f670529d83
Dhruva Garg <dhrgarg@publicisgroupe.net>
2020-04-26 18:11:48 +0530

    fixes

--- src/components/characterList.js
+++ src/components/characterList.js
@@ -5,39 +5,39 @@ import { Row, Col } from "reactstrap";
 
 export const Character = ({ item }) => {
   return (
-    <article class="characterCard__Wrapper">
-      <div data="card header" class="characterCard__ImgWrapper">
-        <div class="card-image">
+    <article className="characterCard__Wrapper">
+      <div data="card header" className="characterCard__ImgWrapper">
+        <div className="card-image">
           <img
             src="https://rickandmortyapi.com/api/character/avatar/62.jpeg"
             alt="Canklanker Thom"
           />
         </div>
-        <div class="characterCard__Title">
-          <h2 class="characterCard__Title__Name">Canklanker Thom</h2>
-          <p class="characterCard__Title__Description">
+        <div className="characterCard__Title">
+          <h2 className="characterCard__Title__Name">Canklanker Thom</h2>
+          <p className="characterCard__Title__Description">
             id: 62 - created 2 years ago
           </p>
         </div>
       </div>
-      <div data="card info" class="characterCard__InfoWrapper">
-        <div class="characterCard__TextWrapper">
+      <div data="card info" className="characterCard__InfoWrapper">
+        <div className="characterCard__TextWrapper">
           <span>STATUS</span>
           <p>Dead</p>
         </div>
-        <div class="characterCard__TextWrapper">
+        <div className="characterCard__TextWrapper">
           <span>SPECIES</span>
           <p>Alien, Gromflomite</p>
         </div>
-        <div class="characterCard__TextWrapper">
+        <div className="characterCard__TextWrapper">
           <span>GENDER</span>
           <p>Male</p>
         </div>
-        <div class="characterCard__TextWrapper">
+        <div className="characterCard__TextWrapper">
           <span>ORIGIN</span>
           <p>Gromflom Prime</p>
         </div>
-        <div class="characterCard__TextWrapper">
+        <div className="characterCard__TextWrapper">
           <span>LAST LOCATION</span>
           <p>unknown</p>
         </div>
