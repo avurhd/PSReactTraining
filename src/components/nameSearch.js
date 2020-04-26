47da64da1bdae8ab08c6236aa4daa91bdd19cca4
Dhruva Garg <dhruva@Dhruvas-MacBook-Pro.local>
2020-04-26 20:18:55 +0530

    sort by added

--- src/components/nameSearch.js
+++ src/components/nameSearch.js
@@ -6,12 +6,16 @@ import {
   addSelected,
   deleteSelected,
   updateInput,
+  updateSortByID,
+  sortByID,
+  SORT_BY_TYPES
 } from "../store/nameFilters";
-import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { Button, Form, FormGroup, Label, Input, Row, Col } from "reactstrap";
 
 export const NameSearch = () => {
   const names = useSelector(selectedNames);
   const nameValue = useSelector(nameFilterInput);
+  const sortByIDValue = useSelector(sortByID);
   const dispatch = useDispatch();
   const submitNameSearch = () => {
     dispatch(addSelected(nameValue));
@@ -31,40 +35,61 @@ export const NameSearch = () => {
             return (
               <div key={"name_" + index} className="chips__item">
                 {item}
-                <span onClick={() => deleteName(item)} className="chips__item__del"> X</span>
+                <span
+                  onClick={() => deleteName(item)}
+                  className="chips__item__del"
+                >
+                  {" "}
+                  X
+                </span>
               </div>
             );
           })}
         </div>
       </div>
-      <Form inline className="nameFilter__FormInline">
-        <FormGroup className="mb-2 mt-2 mb-sm-0">
-          <Label for="char_name_search" className="mr-sm-2">
-            Search By Name
-          </Label>
-          <Input
-            type="text"
-            name="char_name"
-            id="char_name_search"
-            value={nameValue}
-            onChange={(e) => {
-              dispatch(updateInput(e.target.value));
-            }}
-            placeholder=""
-            className=" mr-0 mr-sm-2 mb-2 mb-sm-0"
-          />
+      <Row>
+        <Col xs={12} md={8}>
+          <Form inline className="nameFilter__FormInline">
+            <FormGroup className="mb-2 mt-2 mb-sm-0">
+              <Label for="char_name_search" className="mr-sm-2">
+                Search By Name
+              </Label>
+              <Input
+                type="text"
+                name="char_name"
+                id="char_name_search"
+                value={nameValue}
+                onChange={(e) => {
+                  dispatch(updateInput(e.target.value));
+                }}
+                placeholder=""
+                className=" mr-0 mr-sm-2 mb-2 mb-sm-0"
+              />
 
-          <Button
-            type="submit"
-            onClick={(e) => {
-              e.preventDefault();
-              submitNameSearch();
-            }}
-          >
-            Submit
-          </Button>
-        </FormGroup>
-      </Form>
+              <Button
+                type="submit"
+                onClick={(e) => {
+                  e.preventDefault();
+                  submitNameSearch();
+                }}
+              >
+                Submit
+              </Button>
+            </FormGroup>
+          </Form>
+        </Col>
+        <Col xs={12} md={4}>
+          <Form inine>
+            <FormGroup>
+              <Label>Sort By ID</Label>
+              <Input type="select" name="select" value={sortByIDValue} onChange={(e)=>{dispatch(updateSortByID(Number(e.target.value)))}}>
+                <option value={SORT_BY_TYPES.ASC}>Ascending</option>
+                <option value={SORT_BY_TYPES.DESC}>Descending</option>
+              </Input>
+            </FormGroup>
+          </Form>
+        </Col>
+      </Row>
     </div>
   );
 };
--- src/store/nameFilters.js
+++ src/store/nameFilters.js
@@ -1,8 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+export const SORT_BY_TYPES = {
+  ASC: -1,
+  DESC: 1,
+};
+
 const nameFilters = createSlice({
   name: "nameFilters",
-  initialState: { selected: [], input: "" },
+  initialState: { selected: [], input: "", sortByID: SORT_BY_TYPES.ASC },
   reducers: {
     addSelected: (state, action) => {
       state.selected = [...state.selected, action.payload];
@@ -13,11 +18,15 @@ const nameFilters = createSlice({
     updateInput: (state, action) => {
       state.input = action.payload || "";
     },
+    updateSortByID: (state, action) => {
+      state.sortByID = action.payload;
+    },
   },
 });
 
 const selectedNames = (state) => state.nameFilters.selected || [];
 const nameFilterInput = (state) => state.nameFilters.input || "";
+const sortByID = (state) => state.nameFilters.sortByID;
 
-export const { addSelected, deleteSelected, updateInput } = nameFilters.actions;
-export { nameFilters, selectedNames, nameFilterInput };
+export const { addSelected, deleteSelected, updateInput,updateSortByID } = nameFilters.actions;
+export { nameFilters, selectedNames, nameFilterInput, sortByID };
